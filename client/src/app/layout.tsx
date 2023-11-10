588516c16820863bc3a17124b27f222075e32d14
Maciek Bednarski <mBednar2020@gmail.com>
2023-11-10 18:05:31 +0100

    setup next with tailwind

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: 'Pricer',
+    description: 'Checks prices without effort',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html className='dark' lang='en'>
+            <body className={`${inter.className} bg-dark-mode`}>{children}</body>
+        </html>
+    );
+}
